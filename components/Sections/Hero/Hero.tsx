b9c317f76956d86a10eaa5069cdb10888eac7d30
Ben <benhoeg@outlook.com>
2021-11-18 20:06:23 -0400

    Added link to cta button

--- components/Sections/Hero/Hero.tsx
+++ components/Sections/Hero/Hero.tsx
@@ -1,21 +1,29 @@
-import { ArrowNarrowRightIcon, ArrowRightIcon } from '@heroicons/react/solid'
+import { ArrowNarrowRightIcon } from '@heroicons/react/solid'
 import { FC } from 'react'
 import { StyledHero } from '.'
 import { Button } from '../../Button'
 import { Icon } from '../../Icon'
+import Link from 'next/link'
 
 const Hero: FC = () => {
+
+
+
   return (
     <StyledHero>
       <section className="heroContent">
         <h2 className="greetingTitle">Hi, I'm Ben!</h2>
         <h1 className="ctaTitle">I Build Things For The Web.</h1>
-        <Button type="solid">
-          <span>Check Out My Projects</span>
-          <Icon size={24} fill="#fff">
-            <ArrowNarrowRightIcon />
-          </Icon>
-        </Button>
+        <Link href='/#projects'>
+          <a>
+            <Button type="solid">
+              <span>Check Out My Projects</span>
+              <Icon size={24} fill="#fff">
+                <ArrowNarrowRightIcon />
+              </Icon>
+            </Button>
+          </a>     
+        </Link>     
       </section>
     </StyledHero>
   )
